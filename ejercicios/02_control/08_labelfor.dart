042d592edabe928e488591f03fe9ada10c067539
Illera1999 <illerarodriguez1999@gmail.com>
2024-09-22 21:28:00 +0100

    Sección 2 terminada

--- ejercicios/02_control/08_labelfor.dart
+++ ejercicios/02_control/08_labelfor.dart
@@ -3,7 +3,7 @@ void main(List<String> args) {
   outerLoop:
   for(int i = 0; i < 5; i++){
     print('Valor de i: $i');
-    innerLoop:
+    // innerLoop:
     for(int j = 0; j < 5; j++){
       print('Valor de j: $j');
       if(j == 2)break outerLoop;
